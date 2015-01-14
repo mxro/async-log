cb1c9f9c3b87a88c8523712aaf8d51f63b0f3112
mxro <mxro@nowhere.com>
2015-01-14 14:57:05 +1100

    Adding documentation for async-log Autocommit Wed Jan 14 14:57:05 EST 2015

--- src/main/java/de/mxro/async/log/values/StringLog.java
+++ src/main/java/de/mxro/async/log/values/StringLog.java
@@ -2,8 +2,20 @@ package de.mxro.async.log.values;
 
 import java.util.List;
 
+/**
+ * A log which logs simple strings.
+ * 
+ * @author <a href="http://www.mxro.de">Max Rohde</a>
+ *
+ */
 public interface StringLog {
 
+    /**
+     * <p>
+     * Retrieve the logged entries.
+     * 
+     * @return
+     */
     public List<String> entries();
 
     public void add(String message);
