43c680bdd091248f65489e5b22766d7192157dfb
mxro <mxro@nowhere.com>
2015-01-14 11:57:13 +1100

    Implemeting first version of async-api Autocommit Wed Jan 14 11:57:13 EST 2015

--- src/main/java/de/mxro/async/log/internal/LogFactory.java
+++ src/main/java/de/mxro/async/log/internal/LogFactory.java
@@ -4,6 +4,10 @@ import de.mxro.async.properties.PropertyFactory;
 
 public class LogFactory implements PropertyFactory {
 
+    public LogFactory(final int defaultMaxCapacity) {
+        // TODO Auto-generated constructor stub
+    }
+
     @Override
     public <T> T create(final Class<T> type) {
         // TODO Auto-generated method stub
---
+++ src/main/java/de/mxro/async/log/values/v01/LogData.java
@@ -0,0 +1,36 @@
+package de.mxro.async.log.values.v01;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import de.mxro.async.properties.values.PropertyValue;
+
+public class LogData implements PropertyValue, Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private final int maxCapacity;
+    private final List<String> entries;
+
+    @Override
+    public boolean is(final Class<?> type) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    public List<String> entries() {
+        return this.entries;
+    }
+
+    public void add(final String message) {
+
+    }
+
+    public LogData(final int maxCapacity) {
+        super();
+        this.maxCapacity = maxCapacity;
+        this.entries = new ArrayList<String>(maxCapacity);
+    }
+
+}
