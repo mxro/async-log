095c74fda7c56f8c2c5a4beda0d07deffff99604
mxro <mxro@nowhere.com>
2015-01-14 11:47:03 +1100

    Adding classes.

---
+++ src/main/java/de/mxro/async/log/LogCommon.java
@@ -0,0 +1,26 @@
+package de.mxro.async.log;
+
+import de.mxro.async.log.internal.LogFactory;
+import de.mxro.async.properties.PropertiesCommon;
+import de.mxro.async.properties.PropertyFactory;
+import de.mxro.async.properties.PropertyNode;
+
+/**
+ * <p>
+ * Key methods and factories for Async Log library.
+ * 
+ * @author <a href="http://www.mxro.de">Max Rohde</a>
+ *
+ */
+public class LogCommon extends PropertiesCommon {
+
+    public static PropertyNode create() {
+        return PropertiesCommon.createUnsafe(PropertiesCommon.compositeFactory(PropertiesCommon.defaultFactory(),
+                logFactory()));
+    }
+
+    public static PropertyFactory logFactory() {
+        return new LogFactory();
+    }
+
+}
---
+++ src/main/java/de/mxro/async/log/internal/LogFactory.java
@@ -0,0 +1,13 @@
+package de.mxro.async.log.internal;
+
+import de.mxro.async.properties.PropertyFactory;
+
+public class LogFactory implements PropertyFactory {
+
+    @Override
+    public <T> T create(final Class<T> type) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
