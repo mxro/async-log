2548e3127f34906d577de5cb8ca57c937a5f525f
mxro <mxro@nowhere.com>
2015-01-15 09:13:18 +1100

    Linking logs, properties and metrics to server Autocommit Thu Jan 15 09:13:18 EST 2015

--- src/main/java/de/mxro/async/log/LogsCommon.java
+++ src/main/java/de/mxro/async/log/LogsCommon.java
@@ -6,6 +6,9 @@ import de.mxro.async.properties.PropertiesCommon;
 import de.mxro.async.properties.PropertyFactory;
 import de.mxro.async.properties.PropertyNode;
 import de.mxro.async.properties.PropertyOperation;
+import de.mxro.factories.Configuration;
+import de.mxro.factories.Dependencies;
+import de.mxro.factories.Factory;
 
 /**
  * <p>
@@ -33,6 +36,24 @@ public class LogsCommon extends PropertiesCommon {
 
     public static PropertyOperation<String> string(final String id, final String message) {
         return new WriteEntryOperation(message).setId(id);
+    }
+
+    public static Factory<?, ?, ?> createUnsafeLogsFactory() {
+        return new Factory<PropertyNode, Configuration, Dependencies>() {
+
+            @Override
+            public boolean canInstantiate(final Configuration conf) {
+
+                return conf instanceof LogsConfiguration;
+            }
+
+            @Override
+            public PropertyNode create(final Configuration conf, final Dependencies dependencies) {
+
+                return LogsCommon.createUnsafe(defaultFactory());
+            }
+
+        };
     }
 
 }
--- src/main/java/de/mxro/async/log/LogsConfiguration.java
+++ src/main/java/de/mxro/async/log/LogsConfiguration.java
@@ -2,6 +2,6 @@ package de.mxro.async.log;
 
 import de.mxro.factories.Configuration;
 
-public class LogConfiguration implements Configuration {
+public class LogsConfiguration implements Configuration {
 
 }
